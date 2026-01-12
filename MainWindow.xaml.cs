9cbe658ea8ebfa74c1c492a00ca17194ca5432fa
Vadi <wojtek5521@interia.pl>
2026-01-12 21:49:50 +0100

    Initial commit - SimpleStarter C# WPF

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleStarter
+{
+    public partial class MainWindow : Window
+    {
+        // Nowy obiekt klasy drugiego okna
+        SecondWindow secondWindow = new SecondWindow();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Ustawienie okienka na środku prawej krawędzi ekranu
+            int screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            int screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+            Left = screenWidth - Width;
+            Top = (screenHeight - Height) / 2;
+        }
+
+        private void OpenSecondWindow(object sender, MouseEventArgs e)
+        {
+            int screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            if (!secondWindow.IsVisible)
+            {
+                secondWindow.Show();
+                Left = screenWidth - Width - secondWindow.Width;
+                Icon.Text = "❯";
+            }
+            else
+            {
+                secondWindow.Hide();
+                Left = screenWidth - Width;
+                Icon.Text = "❮";
+            }
+        }
+    }
+}
---
+++ SecondWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleStarter
+{
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+
+            // Ustawienie okienka na środku prawej krawędzi ekranu
+            int screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            int screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+            Left = screenWidth - Width;
+            Top = (screenHeight - Height) / 2;
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Jesteś pewny?", "SimpleStarter", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void RunSimpleCalc(object sender, RoutedEventArgs e)
+        {
+            Process.Start("Apps\\Calc\\SimpleCalc.exe");
+        }
+
+        private void RunSimpleNotePad(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RunSimpleStopWatch(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RunSimplePixelDraw(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RunBrowser(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://www.google.com",
+                UseShellExecute = true
+            });
+        }
+    }
+}
